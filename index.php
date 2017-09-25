67c49a50e9f0b327ee9b6c5ee1e8fd09f74e82a2
TaupinL <lucie.taupin@free.fr>
2017-09-25 11:32:05 +0200

    Petit changement sur l'index

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
-<?php include('header.php');?>
+<?php get_header();?>
 
 <h2>Hello world</h2>
 <h3>Coucou</h3>
 
-<?php include('footer.php');?>
+<?php if(have_posts()):while(have_posts()):
+  the_post();?>
+  <h2><?php the_title();?></h2>
+  <p><?php the_content();?></p>
+<?php endwhile; endif;?>
+
+<?php get_footer();?>
