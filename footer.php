7a0d47972a9c702ba2c4b26a5f8c0c8f850abbca
logingithub <email@github>
2017-09-25 10:23:32 +0200

    1ère version.

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+  <p>C'est le pied... de page !</p>
+  <?php wp_footer();?>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="<?php bloginfo('charset');?>">
+  <meta name="author" content="Lucie TAUPIN et Lucas MATTINA" />
+  <meta name="keywords" content="document theme wordpress" />
+  <title><?php bloginfo('name')?> - <?php bloginfo('description');?></title>
+  <?php wp_head();?>
+</head>
+<body>
+    <header>
+      <h1><?php bloginfo('name');?></h1>
+    </header>
